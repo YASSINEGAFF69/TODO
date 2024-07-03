048778eeaae34396a7b4076108fc64a9828dffa8
Simon Bae <yassineGAFF@gmail.com>
2024-07-03 15:55:22 +0100

    adding dark mode and improving mobile version

--- src/app.js
+++ src/app.js
@@ -6,9 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const newProjectTitleInput = document.getElementById('newProjectTitle');
     const newTodoTitleInput = document.getElementById('newTodoTitle');
     const itemTitle = document.getElementById('itemTitle');
+    const iconBtn = document.querySelector('.icon-button');
     let currentProjectID = null;
     let currentItem = null;
     let projectsData = [];
+
+    
+    const theme = localStorage.getItem('theme');
+
+    theme && document.body.classList.add(theme);
+    const handleThemeToggle = () => {
+      document.body.classList.toggle('dark-mode');
+      if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+        document.querySelector('.icon-button img').setAttribute("src","./images/icon-sun.svg");
+      } else {
+        localStorage.removeItem('theme');
+        document.querySelector('.icon-button img').setAttribute("src","./images/icon-moon.svg");
+      }
+    };
+    iconBtn.addEventListener('click', handleThemeToggle);
+
 
     function renderProjects() {
         const projectsList = document.getElementById('projectsList');
